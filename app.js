10870bc0560c7f87f728a41f521c85a41ba9f8cd
malmossa <malmossa@gmail.com>
2021-03-09 17:36:40 -0800

    Completed

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 class App {
   constructor() {
+    this.notes = [];
+
     this.$form = document.querySelector('#form');
     this.$noteTitle = document.querySelector('.note-title');
+    this.$noteText = document.querySelector('.note-text');
     this.$formButtons = document.querySelector('.form-buttons');
 
     this.addEventListeners();
@@ -10,6 +13,17 @@ class App {
   addEventListeners() {
     document.body.addEventListener('click', event => {
       this.handleFormClick(event);
+    });
+
+    this.$form.addEventListener('submit', event => {
+      event.preventDefault();
+      const title = this.$noteTitle.value;
+      const text = this.$noteText.value;
+      const hasNote = title || text;
+
+      if (hasNote) {
+        this.addNote({title, text});
+      }
     })
   }
 
@@ -24,7 +38,6 @@ class App {
   }
 
   openForm () {
-    this.$form.classList.add('form-open');
     this.$noteTitle.style.display = 'block';
     this.$formButtons.style.display = 'block';
   }
@@ -33,6 +46,18 @@ class App {
     this.$form.classList.remove('form-open');
     this.$noteTitle.style.display = 'none';
     this.$formButtons.style.display = 'none';
+  }
+
+  addNote(note) {
+    const newNote = {
+      title: note.title,
+      text: note.text,
+      createdAt: new Date(),
+      color: 'white',
+      id: this.notes.length > 0 ? this.notes[this.notes.length -1].id + 1 : 1
+    };
+    this.notes = [...this.notes, newNote]
+    console.log(this.notes)
   }
 }
 
